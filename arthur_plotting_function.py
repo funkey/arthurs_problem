6c536e15823bbfcd0e42fea6ab79ec52b3499f66
Jan Funke <jfunke@iri.upc.edu>
2019-07-02 19:21:58 -0400

    Fix order of x y z in plotting function

--- arthur_plotting_function.py
+++ arthur_plotting_function.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as pl
 import time
-
+from secret import secret_process
 
 
 
@@ -10,15 +10,15 @@ def display_dataset(x,y,z):
     n = x.shape[0]
     # compute marginals
 
-    # marginal of y
-    y_pos = np.sum(y)
-    y_neg = n-y_pos
-    print("marginal of y: %f/%f" % (y_pos/n, y_neg/n))
-
     # marginal of z
-    values, counts = np.unique(z, return_counts = True)
+    z_pos = np.sum(z)
+    z_neg = n-z_pos
+    print("marginal of z: %f/%f" % (z_pos/n, z_neg/n))
+
+    # marginal of y
+    values, counts = np.unique(y, return_counts = True)
     for v, c in zip(values, counts):
-        print('marginal of z=%d, c = %f' % (v, c/n))
+        print('marginal of y=%d, c = %f' % (v, c/n))
 
     fig = pl.figure(figsize = (10,15))
     title = 'marginals'
@@ -28,18 +28,18 @@ def display_dataset(x,y,z):
     pl.hist(x, bins = 100)
 
     fig.add_subplot(3,1,2)
-    pl.title('z')
+    pl.title('y')
     pl.bar(x = values, height=counts/n)
 
     fig.add_subplot(3,1,3)
-    pl.title('y')
-    pl.bar(x = [0,1], height=[y_pos, y_neg])
+    pl.title('z')
+    pl.bar(x = [0,1], height=[z_pos, z_neg])
     pl.show()
 
 
 if __name__ == "__main__":
 
-    n = 500
+    n = 10000
     intervention = {}
     x, y, z = secret_process(n, intervention)
     display_dataset(x,y,z)
